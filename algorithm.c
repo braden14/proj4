2b60d0480a976c04b7ebb7acc22c34c4389f06e7
Braden Schroeder <braden14@viper.cs.ksu.edu>
2019-04-30 21:04:55 -0500

    algorithm reads from file

--- algorithm.c
+++ algorithm.c
@@ -3,8 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 
+int numberlines = 100000; // GIVE ME LARGEST LINE VALUE
+int lineLength = 12; // GIVE ME THE TOTAL NUMBER OF LINES
+char fileName[] = "testFile.txt"; // GIVE ME THE NAME OF THE FILE TO READ
 
-char * longestSub(char * word1, int len1, char * word2, int len2)
+// 
+// Function finds the longest substring in two line
+//
+char * longestSub(char * lines1, int len1, char * lines2, int len2)
 {
 	int subCount[len1][len2];
 	int length = 0;
@@ -15,7 +21,7 @@ char * longestSub(char * word1, int len1, char * word2, int len2)
 	{
 		for (int j = 0; j < len2; j++)
 		{
-			if(word1[i] == word2[j])
+			if(lines1[i] == lines2[j])
 			{
 				if(i > 0 && j > 0)
 				{
@@ -43,48 +49,57 @@ char * longestSub(char * word1, int len1, char * word2, int len2)
 		}
 	}
 	char * result = malloc(sizeof(char)*length);
-	strncpy(result, &word1[indexOfI - length + 1], length);
+	strncpy(result, &lines1[indexOfI - length + 1], length);
 	return result;
 }
 
-char ** getWords(int * outNumberwords)
+// 
+// Function gets list of all lines in txt file
+//
+char ** getLines(int * outNumberLines)
 {
-	*outNumberwords = 4;
-	char ** temp = malloc(*outNumberwords*sizeof(char*));
-	temp[0] = malloc(sizeof(char)*4);
-	temp[0] = "cows";
-	temp[1] = malloc(sizeof(char)*7);
-	temp[1] = "plowing";
-	temp[2] = malloc(sizeof(char)*4);
-	temp[2] = "pigs";
-	temp[3] = malloc(sizeof(char)*5);
-	temp[3] = "twigs";
-	temp[3] = malloc(sizeof(char)*5);
-	temp[3] = "twins";
+	*outNumberLines = 0;
+	FILE * file = fopen(fileName, "r");
+	char ** temp = malloc(numberlines*sizeof(char*));
+	char lines[lineLength];
+	while(fgets(lines, lineLength, file) != NULL)
+	{
+		int length = strlen(lines);
+		if(*outNumberLines != lineLength - 1) lines[length-3] = '\0';
+		else lines[length-1];
+		temp[*outNumberLines] = malloc(length*sizeof(char));
+		strcpy(temp[*outNumberLines], lines);
+		(*outNumberLines)++;
+	}
 	return temp;
 }
 
-char ** doLongestSub(char ** words, int * numberWords)
+//
+// Function uses longestSubstring to find longest substring of all lines
+//
+char ** doLongestSub(char ** lines, int * numberlines)
 {
-	char ** subStrings = malloc((*numberWords-1)*sizeof(char*));
-	for(int i = 0; i < *numberWords - 1; i++)
+	char ** subStrings = malloc((*numberlines-1)*sizeof(char*));
+	for(int i = 0; i < *numberlines - 1; i++)
 	{
-		subStrings[i] = longestSub(words[i], strlen(words[i]), words[i+1], strlen(words[i+1]));
+		subStrings[i] = longestSub(lines[i], strlen(lines[i]), lines[i+1], strlen(lines[i+1]));
 	}
 	return subStrings;
 }
 
+//
+// main 
+//
 int main()
 {
-	int * numberWords = malloc(sizeof(int));
-	char ** words = getWords(numberWords);
+	int * numberlines = malloc(sizeof(int));
+	char ** lines = getLines(numberlines);
 	
-	char ** subStrings = doLongestSub(words, numberWords);
+	char ** subStrings = doLongestSub(lines, numberlines);
 	
-	for(int i = 0; i < *numberWords-1; i++)
+	for(int i = 0; i < *numberlines-1; i++)
 	{
 		printf("%s\n", subStrings[i]);
 	}
-	
 }
 
