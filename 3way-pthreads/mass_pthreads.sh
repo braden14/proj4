1129a3e2dd820cbd475b3698a68e7392ba761be8
Braden Schroeder <braden14@viper.cs.ksu.edu>
2019-05-03 00:19:05 -0500

    took out loop and run each

--- 3way-pthreads/mass_pthreads.sh
+++ 3way-pthreads/mass_pthreads.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for i in 1 2 4 8 16
-do 
-	sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=1 --cpus-per-task=$i shell_script_pthreads.sh
-done 	
+
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=1 shell_script_pthreads.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=2 shell_script_pthreads.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=4 shell_script_pthreads.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=8 shell_script_pthreads.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=16 shell_script_pthreads.sh
