6b2c6d9ea3d0461d25def68933a6b3cc05fef32f
Braden Schroeder <braden14@viper.cs.ksu.edu>
2019-05-03 00:28:45 -0500

    attempt to revert

--- 3way-pthreads/mass_pthreads.sh
+++ 3way-pthreads/mass_pthreads.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 
-sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=1 shell_script_pthreads.sh
-sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=2 shell_script_pthreads.sh
-sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=4 shell_script_pthreads.sh
-sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=8 shell_script_pthreads.sh
 sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=16 shell_script_pthreads.sh
